c3a4c750fedd024ae079cb53221933300e579182
smTadeeb <smtadeeb@gmail.com>
2025-09-08 15:36:39 +0300

    String Concatanation understanding in java

---
+++ eg2.java
@@ -0,0 +1,9 @@
+class p2p
+{
+public static void main(String gg[])
+{
+System.out.println("Tadeeb is an "+"Amazing Developer");
+System.out.println("Tadeeb "+ 786);
+System.out.println(786 + " Tadeeb");
+}
+}
