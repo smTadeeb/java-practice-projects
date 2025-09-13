afcf0007d50aff044257c6215af9b99c62f38562
smTadeeb <smtadeeb@gmail.com>
2025-09-13 13:09:06 +0300

    understanding the entry point function working in java

---
+++ practice-questions/understanding_main_nature.java
@@ -0,0 +1,14 @@
+class understanding_main_nature
+{
+public void hello_world()
+{
+System.out.println("Hello World");
+}
+
+public static void main(String gg[])
+{
+understanding_main_nature m;
+m=new understanding_main_nature();
+m.hello_world();
+}
+}
