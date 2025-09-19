cb8c891406e542d8bdfa8c6305f639c9f7c63388
smTadeeb <smtadeeb@gmail.com>
2025-09-19 22:21:11 +0300

    Object variable and class variable as final

---
+++ practice-questions/final_with_object_variable_eg8.java
@@ -0,0 +1,14 @@
+class aaa
+{
+private final int x;
+
+aaa()
+{
+this(10,20);
+}
+aaa(int a , int b)
+{
+x=a;
+System.out.println("Hello");
+}
+}
---
+++ practice-questions/final_with_object_variable_eg9.java
@@ -0,0 +1,17 @@
+class aaa
+{
+
+public final int x;
+
+{
+x=10;
+System.out.println(x);
+}
+
+
+aaa()
+{
+x=20;
+}
+
+}
