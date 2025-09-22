24604af5d82a9b6a63948eec85088af3630a56a3
smTadeeb <smtadeeb@gmail.com>
2025-09-22 16:13:58 +0300

    Abstract class and Abstract methods

---
+++ practice-questions/abstract_class_object_cant_be_created.java
@@ -0,0 +1,16 @@
+abstract class aaa
+{
+abstract public void manual();
+public void sam()
+{
+System.out.println("Hello");
+}
+}
+class abstract_class_object_cant_be_created
+{
+public static void main(String gg[])
+{
+aaa a;
+a=new aaa();
+}
+}
