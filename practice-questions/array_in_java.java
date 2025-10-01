bb3b2aac55ba5a93026df9d75ab3881fd7412982
smTadeeb <smtadeeb@gmail.com>
2025-10-01 20:55:47 +0300

    arrays in java

---
+++ practice-questions/array_in_java.java
@@ -0,0 +1,18 @@
+class array_in_java
+{
+public static void main(String gg[])
+{
+int y;
+int x[];
+x=new int[5];
+// x=55;  <-- it will give error.
+x[0] = 24;
+x[1] = 15;;
+x[2] = 55;
+x[3] = 22;
+x[4] = 69;
+
+x[55] = 85;
+x[-10] = 255;
+}
+}
---
+++ practice-questions/proving_that_java_has_pointers.java
@@ -0,0 +1,20 @@
+class aaa
+{
+public void abdurrehman()
+{
+System.out.println("Hi, I am Abdurrehman the Boss");
+}
+
+}
+
+class psp
+{
+public static void main(String gg[])
+{
+aaa a;
+a=null;
+a.abdurrehman();
+
+}
+}
+
