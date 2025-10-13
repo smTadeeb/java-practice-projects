b8366423040e58b413a3a55e7800b53aa2b872fe
smTadeeb <smtadeeb@gmail.com>
2025-10-13 23:06:37 +0300

    Java is Stringly-typed so I am checking compatibility with byte datatype

---
+++ practice-questions/byte_compatibility_with_all_other_datatype.java
@@ -0,0 +1,76 @@
+class byte_char_compatibility
+{
+public static void main(String gg[])
+{
+byte b;
+char c = 'A';
+b=c;
+}
+}
+
+class byte_float_compatibility          
+{
+public static void main(String gg[])
+{
+byte b;
+float f = 2.3f;
+b=f;
+}
+}
+
+class byte_double_compatibility          
+{
+public static void main(String gg[])
+{
+byte b;
+double d = 2.34;
+b=d;
+}
+}
+
+class byte_byte_compatibility          
+{
+public static void main(String gg[])
+{
+byte b;
+byte c = 23;
+b=c;
+}
+}
+
+class byte_short_compatibility          
+{
+public static void main(String gg[])
+{
+byte b;
+short s = 22;
+b=s;
+}
+}
+
+class byte_int_compatibility          
+{
+public static void main(String gg[])
+{
+byte b;
+int i =234;
+b=i;
+}
+}
+
+
+class byte_long_compatibility          
+{
+public static void main(String gg[])
+{
+byte b;
+long l = 334;
+b=l;
+}
+}
+
+
+
+
+
+
