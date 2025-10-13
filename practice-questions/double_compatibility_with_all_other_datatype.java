ec3b039ae72cac02edfaa3d96508f2135ec5142d
smTadeeb <smtadeeb@gmail.com>
2025-10-14 00:16:38 +0300

    Java is Stringly-typed so I am checking compatibility with double datatype

---
+++ practice-questions/double_compatibility_with_all_other_datatype.java
@@ -0,0 +1,76 @@
+// Works 
+class double_char_compatibility
+{
+public static void main(String gg[])
+{
+double d;
+char c = 'A';
+d=c;
+}
+}
+
+// Work
+class double_float_compatibility          
+{
+public static void main(String gg[])
+{
+double d;
+float f = 2.3f;
+d=f;
+}
+}
+
+// Works
+class double_double_compatibility          
+{
+public static void main(String gg[])
+{
+double d;
+double x = 2.34;
+d=x;
+}
+}
+
+// Works
+class double_byte_compatibility          
+{
+public static void main(String gg[])
+{
+double d;
+byte b = 23;
+d=b;
+}
+}
+
+// Works
+class double_short_compatibility          
+{
+public static void main(String gg[])
+{
+double d;
+short s = 22;
+d=s;
+}
+}
+
+// Work
+class double_int_compatibility          
+{
+public static void main(String gg[])
+{
+double d;
+int i =234;
+d=i;
+}
+}
+
+//Work
+class double_long_compatibility          
+{
+public static void main(String gg[])
+{
+double d;
+long l = 334;
+d=l;
+}
+}
