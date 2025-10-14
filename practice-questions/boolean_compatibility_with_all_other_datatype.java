c03ec344fb7f6ea1db1f4edb95843e30f4f02d09
smTadeeb <smtadeeb@gmail.com>
2025-10-14 09:40:55 +0300

    Java is Stringly-typed so I am checking compatibility with boolean datatype

---
+++ practice-questions/boolean_compatibility_with_all_other_datatype.java
@@ -0,0 +1,76 @@
+//Wont Works 
+class boolean_char_compatibility
+{
+public static void main(String gg[])
+{
+boolean b;
+char c = 'A';
+b=c;
+}
+}
+
+//Wont Work
+class boolean_float_compatibility          
+{
+public static void main(String gg[])
+{
+boolean b;
+boolean f = 2.35f;
+b=f;
+}
+}
+
+// Wont Works
+class boolean_double_compatibility          
+{
+public static void main(String gg[])
+{
+boolean b;
+double d = 2.34;
+b=d;
+}
+}
+
+// Wont Works
+class boolean_byte_compatibility          
+{
+public static void main(String gg[])
+{
+boolean b;
+byte byt = 23;
+b=byt;
+}
+}
+
+// Wont Works
+class boolean_short_compatibility          
+{
+public static void main(String gg[])
+{
+boolean b;
+short s = 23;
+b=s;
+}
+}
+
+// Wont Work
+class boolean_int_compatibility          
+{
+public static void main(String gg[])
+{
+boolean b;
+int i =234;
+b=i;
+}
+}
+
+//Wont Work
+class boolean_long_compatibility          
+{
+public static void main(String gg[])
+{
+boolean b;
+long l = 334;
+b=l;
+}
+}
