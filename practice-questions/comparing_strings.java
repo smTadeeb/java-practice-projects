9bb936ad88576a861c6f27a7a7828a7debc07682
smTadeeb <smtadeeb@gmail.com>
2025-10-15 17:54:16 +0300

    Comparing String's using compareTo()

---
+++ practice-questions/comparing_strings.java
@@ -0,0 +1,15 @@
+class comparing_strings
+{
+public static void main(String gg[])
+{
+String g = "Hello";
+String m = "Alo";
+int x;
+x=g.compareTo(m);
+
+System.out.println(x);
+
+x=m.compareTo(g);
+System.out.println(x);
+}
+}
