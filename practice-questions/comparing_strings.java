0ca850528767e592c80528560c3ab22b5a070ee3
smTadeeb <smtadeeb@gmail.com>
2025-10-15 21:05:15 +0300

    Comparing String's using compareTo()

--- practice-questions/comparing_strings.java
+++ practice-questions/comparing_strings.java
@@ -4,6 +4,8 @@ public static void main(String gg[])
 {
 String g = "Hello";
 String m = "Alo";
+
+/*
 int x;
 x=g.compareTo(m);
 
@@ -11,5 +13,30 @@ System.out.println(x);
 
 x=m.compareTo(g);
 System.out.println(x);
+*/
+/* Errorful code
+if(g>m)
+{
+System.out.println(g + " is heavier than " + m);
+}
+else
+{
+System.out.println(m + " is heavier than " + g);
+}
+*/
+
+if(g.compareTo(m) > 0)
+{
+System.out.println(g + " is heavier than " + m);
+}
+else if(m.compareTo(g) > 0)
+{
+System.out.println(m + " is heavier than " + g);
+}
+else 
+{
+System.out.println(g + " and " + m + " are equal");
+}
+
 }
 }
