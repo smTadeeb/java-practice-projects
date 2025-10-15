07c0a637205cc91e66065832aca5ed978862a148
smTadeeb <smtadeeb@gmail.com>
2025-10-15 12:58:07 +0300

    Clearing one doubt

---
+++ practice-questions/eg_tounderstand_something.java
@@ -0,0 +1,12 @@
+class System
+{
+// something
+}
+
+class eg_to_understand_something
+{
+public static void main(String gg[])
+{
+System.out.println("Hello");  //this will get error as we have defined System class so compiler will search out pointer in our System class not the predefined one
+}
+}
