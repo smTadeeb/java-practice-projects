16c166f35078c5dd013cda491e1d297548e9563f
smTadeeb <smtadeeb@gmail.com>
2025-10-17 16:59:28 +0300

    Interface concepts and Rule7 workout

---
+++ practice-questions/interface_rule7_extends_should_come_before_implements.java
@@ -0,0 +1,27 @@
+interface abc
+{
+
+}
+
+interface pqr
+{
+public void sam();
+}
+
+class aaa
+{
+
+}
+
+class bbb extends aaa implements abc
+{
+}
+
+class ccc extends aaa implements pqr
+{
+public void sam()
+{
+}
+}
+
+
