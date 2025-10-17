f955b9c80d63579484ef93caf9a91fb2bed5df78
smTadeeb <smtadeeb@gmail.com>
2025-10-17 16:13:40 +0300

    Interface concepts and Rule3, Rule4 workout

---
+++ practice-questions/interface_rule3_class_implementing_interface_should_override_abstract_method_of_interface.java
@@ -0,0 +1,9 @@
+class aaa
+{
+public void sam();
+}
+
+class interface_rule3_class_implementing_interface_should_override_abstract_method_of_interface implements aaa
+{
+
+}
---
+++ practice-questions/interface_rule4_class_implementing_interface_should_override_abstract_method_of_interface_or_declare_class_as_abstract.java
@@ -0,0 +1,9 @@
+interface aaa
+{
+public void sam();
+}
+
+// This will compile as we have declared class as abstract
+abstract class interface_rule4_class_implementing_interface_should_override_abstract_method_of_interface_or_declare_class_as_abstract implements aaa
+{
+}
