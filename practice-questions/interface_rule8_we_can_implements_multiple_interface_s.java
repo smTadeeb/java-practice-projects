7b59df5f259efb34edd74563b16ecb280c95b999
smTadeeb <smtadeeb@gmail.com>
2025-10-17 17:13:42 +0300

    Interface concepts and Rule8 workout

---
+++ practice-questions/interface_rule8_we_can_implements_multiple_interface_s.java
@@ -0,0 +1,23 @@
+interface aaa
+{
+public void sam();
+}
+
+interface bbb
+{
+}
+
+interface ccc
+{
+public void tom();
+}
+
+class interface_rule8_we_can_implements_multiple_interface_s implements aaa,bbb,ccc
+{
+public void sam()
+{
+}
+public void tom()
+{
+}
+}
