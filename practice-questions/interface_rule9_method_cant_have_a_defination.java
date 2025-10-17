32f535abae6b6f025680ad4042ba16977d2cb554
smTadeeb <smtadeeb@gmail.com>
2025-10-17 18:22:01 +0300

    Updating Rule9

--- practice-questions/interface_rule9_method_cant_have_a_defination.java
+++
@@ -1,7 +0,0 @@
-interface interface_method_cant_have_a_defination
-{
-public void sam()
-{
-}
-
-}
---
+++ practice-questions/interface_rule9_method_cant_have_a_defination_without_default_keyword.java
@@ -0,0 +1,11 @@
+interface interface_method_cant_have_a_defination_without_default_keyword
+{
+public void sam();
+
+// Using default keyword we can define even a method inside an interface
+default public void tom()
+{
+
+}
+
+}
