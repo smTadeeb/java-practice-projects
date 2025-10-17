3bd1e00de99e73e87bdc673c473e21cf021b8972
smTadeeb <smtadeeb@gmail.com>
2025-10-17 16:09:11 +0300

    Interface concepts and Rule1, Rule2 workout

---
+++ practice-questions/interface_rule1_interface_obj_cant_be_made.java
@@ -0,0 +1,13 @@
+interface abc
+{
+public void sam();
+}
+
+class interface_rule1_interface_obj_cant_be_made
+{
+public static void main(String gg[])
+{
+abc a;
+a=new abc();
+}
+}
---
+++ practice-questions/interface_rule2_interface_cant_be_extended.java
@@ -0,0 +1,13 @@
+interface abc
+{
+public void sam();
+}
+
+class interface_rule2_interface_cant_be_extended extends abc
+{
+public static void main(String gg[])
+{
+abc a;
+//a=new abc();
+}
+}
