a37fe4422a28f0ee8d782eac9242718d72bdb69b
smTadeeb <smtadeeb@gmail.com>
2025-10-17 16:30:25 +0300

    Interface concepts and Rule5 workout

---
+++ practice-questions/interface_rule5_class_implementing_interface_should_override_abstract_method_of_interface_or_declare_class_as_abstract.java
@@ -0,0 +1,16 @@
+interface aaa
+{
+public void sam();
+}
+
+// This will compile as we have overriden the abstract method
+class interface_rule4_class_implementing_interface_should_override_abstract_method_of_interface_or_declare_class_as_abstract implements aaa
+{
+public void sam()
+{
+
+}
+
+}
+
+
