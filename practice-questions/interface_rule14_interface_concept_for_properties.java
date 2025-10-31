04a43754dfe030489ec0a1bf5776e5f129bb1618
smTadeeb <smtadeeb@gmail.com>
2025-10-31 23:47:47 +0300

    Adding interface rule 14

---
+++ practice-questions/interface_rule14_interface_concept_for_properties.java
@@ -0,0 +1,6 @@
+interface aaa
+{
+public int x = 10; // We can have a public property inside a interface but we must initialise it .
+private int y=100; // We cant have private property inside the interface 
+}
+
