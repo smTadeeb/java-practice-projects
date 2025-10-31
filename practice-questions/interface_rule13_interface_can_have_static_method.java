0053238ea4f5a44a939918c66973c4480ec4ad78
smTadeeb <smtadeeb@gmail.com>
2025-10-31 21:52:01 +0300

    interface rule 13

---
+++ practice-questions/interface_rule13_interface_can_have_static_method.java
@@ -0,0 +1,20 @@
+interface aaa
+{
+default public void tom()
+{
+System.out.println("Hello");
+}
+
+static public void sunny()
+{
+System.out.println("Game Over");
+}
+}
+
+class interface_rule13
+{
+public static void main(String gg[])
+{
+aaa.sunny();
+}
+}
